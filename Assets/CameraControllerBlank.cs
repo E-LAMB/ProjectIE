e2a7af9ee4c3e02d377fd8278899fa12e92e43e6
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-01-05 12:24:18 +0000

    Blank world proto

---
+++ Assets/CameraControllerBlank.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerBlank : MonoBehaviour
+{
+
+    public Vector3 offset;
+    public Transform self;
+    public Transform focus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        self.position = focus.position + offset;
+    }
+}
