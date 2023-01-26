f99bdb5012495e2695edd0e904fe481f05e0cbfb
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-01-26 12:42:45 +0000

    Assets

---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+
+    public Texture to_scroll;
+
+    public float scroll_speed;
+    public float current_offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
